5f94a0297ae47bb22e125dd66ef6c03ad8ebbdcd
Lucas Belém <lucas.belem@usto.re>
2025-05-13 19:33:29 -0300

    Ajustes Finais V1

--- src/main/java/com/ts/juridico/infrastructure/config/GoogleDriveConfig.java
+++ src/main/java/com/ts/juridico/infrastructure/config/GoogleDriveConfig.java
@@ -1,17 +1,18 @@
 package com.ts.juridico.infrastructure.config;
 
-
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 
 @Configuration
@@ -20,12 +21,18 @@ public class GoogleDriveConfig {
     private static final String APPLICATION_NAME = "MeuApp";
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
 
+    @Value("${GOOGLE_CREDENTIALS_JSON}")
+    private String credentialsJson;
+
     @Bean
     public Drive googleDrive() throws Exception {
-        // credentials.json deve estar em src/main/resources
-        try (InputStream in = getClass().getResourceAsStream("/credentials.json")) {
-            GoogleCredential credential = GoogleCredential.fromStream(in)
+        try (InputStream in = new ByteArrayInputStream(
+                credentialsJson.getBytes(StandardCharsets.UTF_8))) {
+
+            GoogleCredential credential = GoogleCredential
+                    .fromStream(in)
                     .createScoped(Collections.singleton(DriveScopes.DRIVE_FILE));
+
             return new Drive.Builder(new NetHttpTransport(), JSON_FACTORY, credential)
                     .setApplicationName(APPLICATION_NAME)
                     .build();
